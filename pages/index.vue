bb161535cd0d37cd8c3f651b199d465c035ae70b
Gregory Ross <gregross.dev@gmail.com>
2023-07-27 19:19:38 -0400

    init

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+
+  <main role="main" class="w-full flex flex-col h-screen items-center justify-center z-20">
+    <article class="max-w-lg rounded shadow-lg p-8 bg-white">
+      <h2 class="font-bold text-xl mb-2">Hello, I'm Greg!</h2>
+      <p>Full Stack Dev | Open-Source Enthusiast | Blogger | Ninja</p>
+      <a href="/">Contact Me</a>
+    </article>
+  </main>
+  <Particles />
+</template>
+
+<style scoped></style>
