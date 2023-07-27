208d116cde215100c578aa9457e5e93bf6ed4d72
Gregory Ross <gregross.dev@gmail.com>
2023-07-27 19:36:58 -0400

    fix

--- components/Particles.vue
+++ components/Particles.vue
@@ -1,71 +1,64 @@
-<script lang="ts">
+<script lang="ts" setup>
 import type { Engine } from "tsparticles-engine";
 import { loadSlim } from "tsparticles-slim";
-import {ParticlesComponent} from "vue3-particles"; // loads tsparticles-slim
+import { ParticlesComponent } from "vue3-particles"; // loads tsparticles-slim
 //import { loadFull } from "tsparticles"; // loads tsparticles
-
-export default {
-  components: {ParticlesComponent},
-  data() {
-    return {
-      // using an empty options object will load the default options, which are static particles with no background and 3px radius, opacity 100%, white color
-      // all options can be found here: https://particles.js.org/docs/interfaces/Options_Interfaces_IOptions.IOptions.html
-      options: {
-        background: {
-          color: "#000", // this sets a background color for the canvas
-        },
-        fullScreen: {
-          enable: true, // enabling this will make the canvas fill the entire screen, it's enabled by default
-          zIndex: -1, // this is the z-index value used when the fullScreen is enabled, it's 0 by default
-        },
-        interactivity: {
-          events: {
-            onClick: {
-              enable: true, // enables the click event, it's disabled by default
-              mode: "push", // adds the particles on click
-            },
-            onHover: {
-              enable: true, // enables the mouse hover event, it's disabled by default
-              mode: "repulse", // repulse the particles on mouse hover
-            },
+const options = {
+      background: {
+        color: "#000", // this sets a background color for the canvas
+      },
+      fullScreen: {
+        enable: true, // enabling this will make the canvas fill the entire screen, it's enabled by default
+        zIndex: -1, // this is the z-index value used when the fullScreen is enabled, it's 0 by default
+      },
+      interactivity: {
+        events: {
+          onClick: {
+            enable: true, // enables the click event, it's disabled by default
+            mode: "push", // adds the particles on click
           },
-          modes: {
-            push: {
-              quantity: 10, // number of particles to add on click
-            },
-            repulse: {
-              distance: 300, // distance of the particles from the cursor
-            },
+          onHover: {
+            enable: true, // enables the mouse hover event, it's disabled by default
+            mode: "repulse", // repulse the particles on mouse hover
           },
         },
-        particles: {
-          links: {
-            distance: 200, // maximum distance for linking the particles
-            enable: true, // enabling this will make the particles linked together
+        modes: {
+          push: {
+            quantity: 10, // number of particles to add on click
           },
-          move: {
-            enable: true, // enabling this will make particles move in the canvas
-            speed: { min: 1, max: 5 }, // using a range in speed value will make particles move in a random speed between min/max values, each particle has its own value, it won't change over time by default
-          },
-          number: {
-            value: 100,
-          },
-          opacity: {
-            value: { min: 0.3, max: 0.7 }, // using a different opacity, to have some semitransparent effects
-          },
-          size: {
-            value: { min: 1, max: 3 }, // let's randomize the particles size a bit
+          repulse: {
+            distance: 300, // distance of the particles from the cursor
           },
         },
       },
-    };
-  },
-  methods: {
-    async particlesInit(engine: Engine) {
-      await loadSlim(engine);
-      // await loadFull(engine); // for this sample the slim version is enough, choose whatever you prefer, slim is smaller in size but doesn't have all the plugins and the mouse trail feature
-    },
-  },
+      particles: {
+        links: {
+          distance: 200, // maximum distance for linking the particles
+          enable: true, // enabling this will make the particles linked together
+        },
+        move: {
+          enable: true, // enabling this will make particles move in the canvas
+          speed: { min: 1, max: 5 }, // using a range in speed value will make particles move in a random speed between min/max values, each particle has its own value, it won't change over time by default
+        },
+        number: {
+          value: 100,
+        },
+        opacity: {
+          value: { min: 0.3, max: 0.7 }, // using a different opacity, to have some semitransparent effects
+        },
+        size: {
+          value: { min: 1, max: 3 }, // let's randomize the particles size a bit
+        },
+      },
+    }
+
+const particlesInit = async engine => {
+  //await loadFull(engine);
+  await loadSlim(engine);
+};
+
+const particlesLoaded = async container => {
+  console.log("Particles container loaded", container);
 };
 </script>
 
